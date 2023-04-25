b27037ecc11ad8fe76339789ed328c5e5fd80b71
pragnesh64 <prajapatipragnesh6464@email.com>
2023-04-25 14:49:28 +0530

    commitchange

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ fetch('projects.json')
   .then(response => response.json())
   .then(projects => {
     const projectsContainer = document.getElementById('projects');
-
+    
     // loop through each project and create a card for it
     projects.forEach(project => {
       const card = document.createElement('div');
       card.classList.add('card');
 
-      const image = document.createElement('img');
-      image.src = project.image;
-      image.alt = project.name;
-      card.appendChild(image);
+      // const image = document.createElement('img');
+      // image.src = project.image;
+      // image.alt = project.name;
+      // card.appendChild(image);
 
       const title = document.createElement('h2');
       title.textContent = project.name;
@@ -20,6 +20,15 @@ fetch('projects.json')
       const description = document.createElement('p');
       description.textContent = project.description;
       card.appendChild(description);
+      
+
+
+      const link = document.createElement('a');
+      link.textContent = project.link;
+      card.appendChild(link);
+
+
+
 
       const pricing = document.createElement('div');
       pricing.classList.add('pricing');
